5737111f7b5112c94e430ebba34932ffa031f33d
vhalerie <vll237@nyu.edu>
2016-12-11 14:29:46 -0500

    Update sketch_1.js
    Revised Moire Pattern sketch

--- Assignment-Week3/sketch_1.js
+++ Assignment-Week3/sketch_1.js
@@ -1,38 +1,10 @@
 function setup() {
-  var cnv = createCanvas(500,500);
-  var w=(windowWidth - width)/2;
-  var h=(windowHeight - height)/2;
-  cnv.position(w,h);
+  createCanvas(windowWidth,windowHeight);
   background(255);
-}
-
-function draw(){
-  background(255);
-	var x=0;
-  var y=0;
-  var planeWidth=300;
-	//uncomment this code and play around
-	// push();
-  // rotate( -map(mouseX,0,width,0,TWO_PI)); 
-  beginShape();
-  for(var i=0; i< planeWidth; i++){
-    
-    vertex(x,y);
-    vertex(x,planeWidth);
-    x+=3;
-    
-    vertex(x+20, y+100);
-    vertex(x+30, planeWidth);
-    y+=3;
-  }
-  endShape();
-  //uncomment this code and play around
-	// pop();
-	
-	
-	//try uncommenting this code and playing around
-	//push();
-  //rotate(map(mouseX,0,width,0,TWO_PI));
+  
+  //try uncommenting this code and playing around
+	push();
+  rotate(map(mouseX,0,width,0,TWO_PI));
   beginShape(LINES);
   var a=0, b=0;
   for(var j=0; j<height;j++){
@@ -44,5 +16,32 @@ function draw(){
   }
   endShape();
 	//try uncommenting this code
-	//pop();
+	pop();
+}
+
+function draw(){
+  translate(width/2, height/2);
+	var x=0;
+  var y=0;
+  var planeWidth=300;
+  
+	//uncomment this code and play around
+	push();
+  rotate(-map(mouseX,0,width,0,TWO_PI)); 
+  beginShape();
+  for(var i=0; i< planeWidth; i+=5){
+    vertex(x,y);
+    vertex(x,planeWidth);
+    x+=3;
+    
+    vertex(x+100, y+100);
+    vertex(x+200, planeWidth);
+    y+=3;
+  }
+  endShape();
+  //uncomment this code and play around
+	pop();
+	
+	
+	
 }
